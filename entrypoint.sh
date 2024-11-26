2a6b15015dde470009de198b507e6b0f152a25cf
Dafydd Jones <dafydd@techneg.it>
2024-11-26 01:34:41 +0000

    safe dir

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -l
 
-git config --global --add safe.directory .
+git config --global --add safe.directory /github/workspace
 pre-commit run --all-files --color always --verbose
 cat /github/home/.cache/pre-commit/pre-commit.log
